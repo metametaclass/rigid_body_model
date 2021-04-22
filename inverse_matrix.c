4659749ccb960c8c9ef24a92e5061ab3c37ff54b
metametaclass <metametaclass@gmail.com>
2021-04-22 08:56:00 +0300

    added lapacke matrix inversion

---
+++ inverse_matrix.c
@@ -0,0 +1,34 @@
+//#include <gsl/gsl_blas.h>
+#include <gsl/gsl_errno.h>
+#include <lapacke.h>
+
+#include "wmq_error.h"
+
+int inverse_matrix(double m[], int size) {
+    int pivot[size];
+    int err;
+    dgetrf_(&size, &size, m, &size, pivot, &err);
+    if (err < 0) {
+        WMQ_LOG_ERROR("dgetrf_: invalid argument %d", -err);
+        return WMQE_EXTERNAL_ERROR;
+    }
+    if (err > 0) {
+        WMQ_LOG_ERROR("dgetrf_: singular decomposition");
+        return WMQE_EXTERNAL_ERROR;
+    }
+
+    int lwork = size;
+    double work[lwork];
+
+    dgetri_(&size, m, &size, pivot, work, &lwork, &err);
+    if (err < 0) {
+        WMQ_LOG_ERROR("dgetri_: invalid argument %d", -err);
+        return WMQE_EXTERNAL_ERROR;
+    }
+    if (err > 0) {
+        WMQ_LOG_ERROR("dgetri_: singular decomposition");
+        return WMQE_EXTERNAL_ERROR;
+    }
+
+    return 0;
+}
---
+++ inverse_matrix.h
@@ -0,0 +1,4 @@
+#pragma once
+
+//inverse N*N matrix in-place
+int inverse_matrix(double m[], int size);
